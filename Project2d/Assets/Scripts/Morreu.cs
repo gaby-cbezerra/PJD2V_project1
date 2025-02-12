3cde5e2645abb98df1ab3c16525eaa555f274c26
gaby-cbezerra <135763397+gaby-cbezerra@users.noreply.github.com>
2025-02-12 08:34:27 -0300

    Modificações no script Morreu
    Adicionando passagem entre cenas e reinicialização para primeira cena.

--- Project2d/Assets/Scripts/Morreu.cs
+++ Project2d/Assets/Scripts/Morreu.cs
@@ -4,13 +4,14 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class Moreu : MonoBehaviour
+public class Morreu : MonoBehaviour
 {
+    [SerializeField]int cena; 
     private void OnCollisionEnter2D(Collision2D other)
     {
         if (other.gameObject.tag == "Player")
         {
-            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            SceneManager.LoadScene(cena);
         }
     }
 
